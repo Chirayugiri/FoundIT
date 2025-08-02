b8e00cdd18253d6f81cbd99ba0fda18d85c93524
chirayu giri <chirayugiri149@gmail.com>
2025-08-02 18:08:29 +0530

    final update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,33 +6,9 @@ function Navbar() {
         <div>
             <header className="navbar">
                 <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-                    <div className="text-2xl font-bold text-blue-600">
+                    <div className="font-bold text-blue-600" style={{ fontSize: '1.9rem' }}>
                         FoundIT
                     </div>
-                    <nav className="space-x-6">
-                        {/* <a className="text-gray-600 hover:text-blue-600" href="#">
-                            Home
-                        </a> */}
-                        {/* <a className="text-gray-600 hover:text-blue-600" href="#">
-                            Lost Items
-                        </a>
-                        <a className="text-gray-600 hover:text-blue-600" href="#">
-                            Found Items
-                        </a> */}
-                        {/* <button
-                            className="text-blue-600 hover:text-blue-700 cursor-pointer bg-transparent border-none p-0"
-                            onClick={() => {
-                                localStorage.clear();
-                                window.location.href = "/login";
-                            }}
-                            >
-                            Logout
-                        </button> */}
-
-                        {/* className="bg-blue-600 text-white px-4 py-2 rounded" */}
-
-
-                    </nav>
                     <div className="space-x-4">
                         <Link to='/myclaimrequest' className="text-gray-600 hover:text-blue-600">
                             My Requests
